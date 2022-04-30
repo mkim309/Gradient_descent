838f81713df7aaa7a960a4b0e27dd31202c628d3
mkim309 <59399158+mkim309@users.noreply.github.com>
2022-04-29 21:14:34 -0400

    Create gd.py

---
+++ gd.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def gradient_descent(start, gradient, learn_rate, max_iter, tol=0.01):
+  steps = [start] # history tracking
+  x = start
+  
+  
+  
+  
+  
+  for _ in range(max_iter):
+    diff = learn_rate*gradient(x)
+    if np.abs(diff)<tol:
+      break    
+    x = x - diff
+    steps.append(x) # history tracing
+
+  return steps, x
